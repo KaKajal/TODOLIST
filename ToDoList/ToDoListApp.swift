8b43eb2cb50e60ea461863b9a56cff43cd55b8ed
“kaKajal” <kakajal@deloitte.com>
2024-06-30 14:54:59 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Kajal, Kajal on 30/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
